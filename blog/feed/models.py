c08de55fbb8c75738219f90a7180bcedfc509ac3
ulises <ulisespallares8@gmail.com>
2021-11-29 02:29:12 -0300

    cambios en models

--- blog/feed/models.py
+++ blog/feed/models.py
@@ -5,19 +5,18 @@ from  django.contrib.auth.models import User
 
 class usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    usuario_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_fk = models.OneToOneField(User, on_delete=models.CASCADE)
     tipo_usuario = models.CharField(max_length=10 )
 
     def __str__(self):
-        salida = '{} {} {}'.format(self.id, self.usuario_fk, self.tipo_usuario)
-        return salida
+        return self.usuario_fk.username
 
 class post(models.Model):
     posteador = models.ForeignKey(usuario, on_delete=models.CASCADE, null=True)
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     creado_en = models.DateTimeField(auto_now_add=True)
-    #actualizado_en = models.DateTimeField(auto_now=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
     me_gusta = models.IntegerField(default=0)
     no_megusta = models.IntegerField(default=0)
     #imagen = models.ImageField(upload_to='post_images', blank=True)
@@ -27,14 +26,14 @@ class post(models.Model):
         return self.titulo
 
 class comentario(models.Model):
-    post = models.OneToOneField(post, on_delete=models.CASCADE)
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
     comentador = models.ForeignKey(usuario, on_delete=models.CASCADE, null=True)
     contenido = models.TextField()
     creado_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.contenido
+        salida = '{} {} {} {} {}'.format(self.id, self.post, self.comentador, self.contenido, self.creado_en)
+        return salida
 
 
-# Create your models here.
