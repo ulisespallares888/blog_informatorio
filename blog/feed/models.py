93b70725c47b7d7c7511e46f86da4c654036329c
ulises <ulisespallares8@gmail.com>
2021-11-29 02:01:22 -0300

    models updates

--- blog/feed/models.py
+++ blog/feed/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from  django.contrib.auth.models import User
 
+
+
+class usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+    tipo_usuario = models.CharField(max_length=10 )
+
+    def __str__(self):
+        salida = '{} {} {}'.format(self.id, self.usuario_fk, self.tipo_usuario)
+        return salida
+
 class post(models.Model):
+    posteador = models.ForeignKey(usuario, on_delete=models.CASCADE, null=True)
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     creado_en = models.DateTimeField(auto_now_add=True)
-    actualizado_en = models.DateTimeField(auto_now=True)
+    #actualizado_en = models.DateTimeField(auto_now=True)
     me_gusta = models.IntegerField(default=0)
     no_megusta = models.IntegerField(default=0)
     #imagen = models.ImageField(upload_to='post_images', blank=True)
@@ -15,7 +27,8 @@ class post(models.Model):
         return self.titulo
 
 class comentario(models.Model):
-    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    post = models.OneToOneField(post, on_delete=models.CASCADE)
+    comentador = models.ForeignKey(usuario, on_delete=models.CASCADE, null=True)
     contenido = models.TextField()
     creado_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
@@ -24,14 +37,4 @@ class comentario(models.Model):
         return self.contenido
 
 
-class usuario(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    usuario_fk = models.ForeignKey(User, on_delete=models.CASCADE, null=True,   default=None)
-    posts = models.ForeignKey(post , on_delete=models.CASCADE, null=True,default=None)
-    comentarios = models.ForeignKey(comentario , on_delete=models.CASCADE, null=True, default=None)
-    tipo_usuario = models.CharField(max_length=10)
-
-    def __str__(self):
-        salida = '{} {} {}'.format(self.id, self.usuario_fk, self.tipo_usuario)
-        return salida
 # Create your models here.
