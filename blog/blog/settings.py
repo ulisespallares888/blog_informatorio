c59a51d01869024458f5b1e26e29853b0b228e71
ulises <ulisespallares8@gmail.com>
2021-11-30 10:57:03 -0300

    dsa

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-o4ir*xlcqqh9k+wt4g#u7)%eq^g)z*6c)dwla93tvxz!kty091'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
