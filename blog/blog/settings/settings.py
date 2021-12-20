8e7e6bc2da99db8145b71499042e98ec55c82e2e
ulisespallares888 <54750620+ulisespallares888@users.noreply.github.com>
2021-12-20 19:39:01 -0300

    Update settings.py

--- blog/blog/settings/settings.py
+++ blog/blog/settings/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,4 +149,3 @@ LOGOUT_REDIRECT_URL = 'feed'
 
 DISABLE_COLLECTSTATIC = 1
 
-django_heroku.settings(locals())
