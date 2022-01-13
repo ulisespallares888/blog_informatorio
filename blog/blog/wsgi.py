16a89916984155eef7129d50d02232d164b55a41
ulises <ulisespallares8@gmail.com>
2022-01-13 19:11:22 -0300

    asd

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -14,6 +14,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
 application = DjangoWhiteNoise(application)
