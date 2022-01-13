854c386c63c64ee83f246129fe7ab815fda93f09
ulises <ulisespallares8@gmail.com>
2022-01-13 17:58:33 -0300

    ASDS

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'checkstatic/static/')
+STATIC_FILES = [
+    'css',
+    'js',
+    'img',
 ]
 
 
