d9275e1e2bfe088c2e59dcfc25f57b696d5653a9
ulises <ulisespallares8@gmail.com>
2022-01-13 18:48:58 -0300

    asd

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,13 +80,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -143,3 +150,4 @@ LOGOUT_REDIRECT_URL = 'feed'
 
 DISABLE_COLLECTSTATIC = 1
 
+django_heroku.settings(locals())
