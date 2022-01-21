ac0298e7cf5acaf10402181f9c02e68367338b3c
ulises <ulisespallares8@gmail.com>
2022-01-21 12:42:23 -0300

    d

--- blog/blog/settings/production.py
+++ blog/blog/settings/production.py
@@ -26,7 +26,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATIC_URL = 'https://django-pbpostgres.herokuapp.com/static/'
-MEDIA_URL = 'https://django-pbpostgres.herokuapp.com/media/'
+MEDIA_URL = 'https://django-pbpostgres.herokuapp.com/'
 
 DISABLE_COLLECTSTATIC = 1
 
