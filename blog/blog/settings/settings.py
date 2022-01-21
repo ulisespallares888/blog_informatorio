0d2ca5c71f169e9088779f1a1a6029df6224903b
ulises <ulisespallares8@gmail.com>
2022-01-21 12:10:50 -0300

    sdf

--- blog/blog/settings/settings.py
+++ blog/blog/settings/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'checkstatic/static/')
+   os.path.join(BASE_DIR, 'feed/static')
 ]
 
 
