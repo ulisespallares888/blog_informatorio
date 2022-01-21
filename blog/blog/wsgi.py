8af01c3dc8425382e1cfe1217e60b87c0cb0e89e
ulises <ulisespallares8@gmail.com>
2022-01-21 12:08:00 -0300

    sd

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.production')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
